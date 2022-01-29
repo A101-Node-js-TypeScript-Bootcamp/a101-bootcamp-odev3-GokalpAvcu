5b993a8d13799971f6baa16ac1a27fa37e7e0aa2
Gökalp Avcu <72872909+GokalpAvcu@users.noreply.github.com>
2022-01-29 14:31:45 +0300

    Update productController.js

--- productController.js
+++ productController.js
@@ -6,12 +6,10 @@ exports.getAllProducts = async (req,res) => { //get All Products
     res.send(response);
 };
 
-
 exports.getById = async (req,res) => { //getById Products
     const response = await Products.getById(req.params);
     res.send(response);
 };
-
 
 exports.getByDiscount = async (req,res) => { //getByDiscount Products
     const response = await Products.getByDiscount(req.params, req.body);
@@ -19,19 +17,16 @@ exports.getByDiscount = async (req,res) => { //getByDiscount Products
     res.send(response);
 };
 
-
 exports.createProduct = async (req,res) => { //create Product
     const response = await Products.add(req.body);
     console.log(response);
     res.send(response);
 };
 
-
 exports.updateProduct = async (req,res) => { //update Product
     const response = await Products.update(req.params,req.body);
     res.send(response);
 };
-
 
 exports.deleteProduct = async (req,res) => { //delete Product
     const response = await Products.delete(req.params);
